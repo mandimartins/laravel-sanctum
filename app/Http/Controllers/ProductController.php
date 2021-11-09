85155d08349fc7784a2d61a26ae3c1b97da55ba6
mandimartins <mandimartins23@gmail.com>
2021-11-09 16:16:30 -0300

    feat: add update, delete and search routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -54,7 +54,10 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+         $product = Product::find($id);
+         $product->update($request->all());
+
+         return $product;
     }
 
     /**
@@ -65,6 +68,16 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return Product::destroy($id);
+    }
+    /**
+     * Search for a name
+     *
+     * @param  str $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Product::where('name', "like", "%" . $name . "%")->get();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/products", [ProductController::class,"index"]);
 Route::post("/products", [ProductController::class, "store"]);
-
+Route::get("/products/{id}", [ProductController::class, "show"]);
+Route::put("/products/{id}", [ProductController::class, "update"]);
+Route::delete("/products/{id}", [ProductController::class, "destroy"]);
+Route::get("/products/search/{name}", [ProductController::class, "search"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
